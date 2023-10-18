16cfc796003e1765cfe57bebf11b6c1483c8e2af
Noe <noe.delvaux@free.fr>
2023-10-18 15:26:30 +0200

    update taille N

--- uyqfdouaz.c
+++ uyqfdouaz.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char board[3][3]; // Le tableau du jeu
+#define MAX_SIZE 10
+
+char board[MAX_SIZE][MAX_SIZE]; // Le tableau du jeu
 
 // Initialiser le tableau
 void initialize_board(int N) {
@@ -12,24 +14,34 @@ void initialize_board(int N) {
     }
 }
 
+//Sélectionner taille du jeu
+int game_size(){
+    int taille;
+    printf("Quelle taille de jeu voulez vous ?\n");
+    scanf("%d",&taille);
+    return taille;
+}
+
 // Afficher le tableau
 void print_board(int N) {
     for (int k=0;k<N;k++)
     {
-        printf("----\n");
+        printf("----");
     }
+    printf("-\n");
     
     for (int i = 0; i < N; i++) {
         for (int j = 0;j<N;j++)
         {
             if (j==(N-1)){printf("| %c |\n", board[i][j]);}
-            else{printf("| %c", board[i][j]);}
+            else{printf("| %c ", board[i][j]);}
             
         }
         for (int l=0;l<N;l++)
         {
-            printf("----\n");
+            printf("----");
         }
+        printf("-\n");
     }
 }
 
@@ -71,13 +83,13 @@ int check_game_over() {
 }
 
 // Jouer un tour
-void play_turn(char player) {
+void play_turn(char player, int N) {
     int row, col;
     printf("Joueur %c, entrez la ligne et la colonne : ", player);
     scanf("%d %d", &row, &col);
-    if (row < 0 || row > 2 || col < 0 || col > 2 || board[row][col] != ' ') {
-        printf("Coup invalide. Réessayez.\n");
-        play_turn(player);
+    if (row < 0 || row > N-1 || col < 0 || col > N-1 || board[row][col] != ' ') {
+        printf("Coup invalide. Reessayez.\n");
+        play_turn(player, N);
     } else {
         board[row][col] = player;
     }
@@ -86,11 +98,16 @@ void play_turn(char player) {
 // Fonction principale
 int main() {
     char player = 'X';
-    int game_over = 0;
-    initialize_board(5);
+    int game_over = 0, board_size;
+    board_size=game_size();
+    while (board_size >= MAX_SIZE){
+        printf("Essaie plus petit (inferieur a %d)\n",MAX_SIZE);
+        board_size = game_size();
+    }
+    initialize_board(board_size);
     while (!game_over) {
-        print_board(5);
-        play_turn(player);
+        print_board(board_size);
+        play_turn(player, board_size);
         game_over = check_game_over();
         if (game_over == 1) {
             printf("Joueur %c a gagné !\n", player);
@@ -100,6 +117,6 @@ int main() {
             player = (player == 'X') ? 'O' : 'X';
         }
     }
-    print_board(5);
+    print_board(board_size);
     return 0;
 }
